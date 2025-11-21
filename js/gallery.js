4bbf739910d9b563b3c7b1c02bd561c7938dc758
KBland14 <165948373+KBland14@users.noreply.github.com>
2025-11-20 19:03:18 -0500

    Refactor image preview functions in gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,5 @@
-function upDate(previewPic){
-    document.getElementById("image").innerHTML=previewPic.alt
-    document.getElementById("image").style.background="url("+previewPic.src+")" ;
-
-}
-
-function unDo(){
-    document.getElementById("image").style.background="#8e68ff";
-    document.getElementById('image').innerHTML= "Hover over an image below to display here."
- 
-}
-
 function myPic () {
-   myImages = document.querySelectorAll (".preview")
+  let myImages = document.querySelectorAll (".preview")
     for (var i = 0; i < myImages.length; i++) {
 
         console.log (myImages[i])
@@ -20,4 +8,17 @@ function myPic () {
 
         
     }
+}
+
+
+function upDate(previewPic){
+    document.getElementById("image").innerHTML=previewPic.alt
+    document.getElementById("image").style.background="url("+previewPic.src+")" ;
+    console.log (upDate);
+}
+
+function unDo(){
+    document.getElementById("image").style.background="#8e68ff";
+    document.getElementById('image').innerHTML= "Hover over an image below to display here."
+    console.log (unDo);
 }
